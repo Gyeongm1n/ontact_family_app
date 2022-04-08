799d642a9cfe281411e6425428dcd2c436bde0bb
Gyeongm1n <affirming_km@naver.com>
2022-04-08 11:48:55 +0900

    로그인, 회원가입 center 적용

--- lib/components/LoginScreen.dart
+++ lib/components/LoginScreen.dart
@@ -49,137 +49,139 @@ class _LoginScreenState extends State<LoginScreen> {
               height: MediaQuery.of(context).size.height - 170,
               width: MediaQuery.of(context).size.width - 40,
               child: SingleChildScrollView(
-                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2 - 230),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '로그인',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2.0,
+                //padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2 - 230),
+                child: Center(
+                  heightFactor: 2.1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '로그인',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.0,
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              key: ValueKey(1),
-                              validator: (value) {
-                                if (value!.isEmpty || value.length < 4) {
-                                  return '아이디를 확인하세요.';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                userId = value!;
-                              },
-                              onChanged: (value) {
-                                userId = value;
-                              },
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.account_circle,
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                key: ValueKey(1),
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return '아이디를 확인하세요.';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  userId = value!;
+                                },
+                                onChanged: (value) {
+                                  userId = value;
+                                },
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.account_circle,
                                     color: Colors.grey,
                                   ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  hintText: '아이디를 입력하세요.',
+                                  hintStyle:
+                                      TextStyle(fontSize: 14, color: Colors.grey),
+                                  contentPadding: EdgeInsets.all(10.0),
                                 ),
-                                hintText: '아이디를 입력하세요.',
-                                hintStyle:
-                                    TextStyle(fontSize: 14, color: Colors.grey),
-                                contentPadding: EdgeInsets.all(10.0),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              key: ValueKey(2),
-                              validator: (value) {
-                                if (value!.isEmpty || value.length < 4) {
-                                  return '비밀번호를 확인하세요.';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                userPassword = value!;
-                              },
-                              onChanged: (value) {
-                                userPassword = value;
-                              },
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.password,
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                obscureText: true,
+                                key: ValueKey(2),
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return '비밀번호를 확인하세요.';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  userPassword = value!;
+                                },
+                                onChanged: (value) {
+                                  userPassword = value;
+                                },
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.password,
                                     color: Colors.grey,
                                   ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  hintText: '비밀번호를 입력하세요.',
+                                  hintStyle:
+                                      TextStyle(fontSize: 14, color: Colors.grey),
+                                  contentPadding: EdgeInsets.all(10.0),
                                 ),
-                                hintText: '비밀번호를 입력하세요.',
-                                hintStyle:
-                                    TextStyle(fontSize: 14, color: Colors.grey),
-                                contentPadding: EdgeInsets.all(10.0),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 40,
-                      height: 40,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          _tryValidation(context);
-                        },
-                        style: ButtonStyle(),
-                        child: Text(
-                          '로그인',
-                          style: TextStyle(
-                              fontSize: 15.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          '아직 회원이 아닌가요?',
-                          style: TextStyle(fontSize: 15, letterSpacing: 2.0),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(
-                                fontSize: 15,
-                                decoration: TextDecoration.underline),
+                            ],
                           ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/register');
-                          },
-                          child: Text('회원가입'),
                         ),
-                      ],
-                    )
-                  ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 40,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            _tryValidation(context);
+                          },
+                          style: ButtonStyle(),
+                          child: Text(
+                            '로그인',
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            '아직 회원이 아닌가요?',
+                            style: TextStyle(fontSize: 15, letterSpacing: 2.0),
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: TextStyle(
+                                  fontSize: 15,
+                                  decoration: TextDecoration.underline),
+                            ),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/register');
+                            },
+                            child: Text('회원가입'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/components/RegisterScreen.dart
+++ lib/components/RegisterScreen.dart
@@ -47,170 +47,173 @@ class _RegisterScreenState extends State<RegisterScreen> {
               height: MediaQuery.of(context).size.height - 170,
               width: MediaQuery.of(context).size.width - 40,
               child: SingleChildScrollView(
-                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2 - 260),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '회원가입',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2.0,
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              key: ValueKey(1),
-                              validator: (value) {
-                                if (value!.isEmpty || value.length < 4) {
-                                  return '아이디를 확인하세요.';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                userId = value!;
-                              },
-                              onChanged: (value) {
-                                userId = value;
-                              },
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.account_circle,
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                hintText: '아이디를 입력하세요.',
-                                hintStyle:
-                                    TextStyle(fontSize: 14, color: Colors.grey),
-                                contentPadding: EdgeInsets.all(10.0),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              key: ValueKey(2),
-                              validator: (value) {
-                                if (value!.isEmpty || value.length < 4) {
-                                  return '비밀번호를 확인하세요.';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                userPassword = value!;
-                              },
-                              onChanged: (value) {
-                                userPassword = value;
-                              },
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.password,
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                hintText: '비밀번호를 입력하세요.',
-                                hintStyle:
-                                    TextStyle(fontSize: 14, color: Colors.grey),
-                                contentPadding: EdgeInsets.all(10.0),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              key: ValueKey(3),
-                              validator: (value) {
-                                if (value!.isEmpty || value.length < 4) {
-                                  return '토큰을 확인하세요.';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                userToken = value!;
-                              },
-                              onChanged: (value) {
-                                userToken = value;
-                              },
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.token,
-                                  color: Colors.grey,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                hintText: '토큰을 입력하세요.',
-                                hintStyle:
-                                    TextStyle(fontSize: 14, color: Colors.grey),
-                                contentPadding: EdgeInsets.all(10.0),
-                              ),
-                            ),
-                          ],
+                //padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2 - 260),
+                child: Center(
+                  heightFactor: 1.75,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '회원가입',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.0,
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 40,
-                      height: 40,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          _tryValidation();
-                        },
-                        style: ButtonStyle(),
-                        child: Text(
-                          '회원가입',
-                          style: TextStyle(
-                              fontSize: 15.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          '로그인 정보가 있으신가요?',
-                          style: TextStyle(fontSize: 15, letterSpacing: 2.0),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(
-                                fontSize: 15,
-                                decoration: TextDecoration.underline),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                key: ValueKey(1),
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return '아이디를 확인하세요.';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  userId = value!;
+                                },
+                                onChanged: (value) {
+                                  userId = value;
+                                },
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.account_circle,
+                                    color: Colors.grey,
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  hintText: '아이디를 입력하세요.',
+                                  hintStyle:
+                                      TextStyle(fontSize: 14, color: Colors.grey),
+                                  contentPadding: EdgeInsets.all(10.0),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                obscureText: true,
+                                key: ValueKey(2),
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return '비밀번호를 확인하세요.';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  userPassword = value!;
+                                },
+                                onChanged: (value) {
+                                  userPassword = value;
+                                },
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.password,
+                                    color: Colors.grey,
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  hintText: '비밀번호를 입력하세요.',
+                                  hintStyle:
+                                      TextStyle(fontSize: 14, color: Colors.grey),
+                                  contentPadding: EdgeInsets.all(10.0),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                obscureText: true,
+                                key: ValueKey(3),
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return '토큰을 확인하세요.';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  userToken = value!;
+                                },
+                                onChanged: (value) {
+                                  userToken = value;
+                                },
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.token,
+                                    color: Colors.grey,
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  hintText: '토큰을 입력하세요.',
+                                  hintStyle:
+                                      TextStyle(fontSize: 14, color: Colors.grey),
+                                  contentPadding: EdgeInsets.all(10.0),
+                                ),
+                              ),
+                            ],
                           ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/');
-                          },
-                          child: Text('로그인'),
                         ),
-                      ],
-                    )
-                  ],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 40,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            _tryValidation();
+                          },
+                          style: ButtonStyle(),
+                          child: Text(
+                            '회원가입',
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            '로그인 정보가 있으신가요?',
+                            style: TextStyle(fontSize: 15, letterSpacing: 2.0),
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: TextStyle(
+                                  fontSize: 15,
+                                  decoration: TextDecoration.underline),
+                            ),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/');
+                            },
+                            child: Text('로그인'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
